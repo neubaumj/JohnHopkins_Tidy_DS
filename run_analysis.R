e858ed22acf0cd93be2084021ba904e0e7b208f4
neubaumj <jeffn2000@yahoo.com>
2020-12-04 17:09:04 -0500

    Second version

--- run_analysis.R
+++ run_analysis.R
@@ -11,7 +11,7 @@
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set
 #    with the average of each variable for each activity and each subject.
-
+#
 # Steps
 # .\\UCI HAR Dataset\\test
 # .\\UCI HAR Dataset\\train
